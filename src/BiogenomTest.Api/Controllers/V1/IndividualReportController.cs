5ea634dd6f5063d619c48f376022989423d1a35c
olegg <s.gelo2004@gmail.com>
2025-07-11 22:39:36 +0300

    feat: add controller and GetUserIntakes endpoint

---
+++ src/BiogenomTest.Api/Controllers/V1/IndividualReportController.cs
@@ -0,0 +1,15 @@
+﻿using BiogenomTest.Application.BiogenomTest.Queries.GetDailyIntake;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BiogenomTest.Api.Controllers.V1;
+
+public class IndividualReportController(IMediator mediator) : ControllerBase
+{
+    [HttpGet("daily-intake")]
+    public async Task<IActionResult> GetUserIntakes([FromQuery] GetDailyIntakesQuery request)
+    {
+        var result = await mediator.Send(request);
+        return Ok(result);
+    }
+}
