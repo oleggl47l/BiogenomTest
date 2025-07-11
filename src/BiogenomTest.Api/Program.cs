937866522ac9c953666ede8bfdf75b836966703f
olegg <s.gelo2004@gmail.com>
2025-07-11 22:49:20 +0300

    feat: add serilog

--- src/BiogenomTest.Api/Program.cs
+++ src/BiogenomTest.Api/Program.cs
@@ -1,6 +1,7 @@
 using System.Text.Json.Serialization;
 using BiogenomTest.Application.Extensions;
 using BiogenomTest.Infrastructure.Extensions;
+using Serilog;
 
 namespace BiogenomTest.Api;
 
@@ -8,30 +9,51 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
-        var configuration = builder.Configuration;
+        Log.Logger = new LoggerConfiguration()
+            .Enrich.FromLogContext()
+            .ReadFrom.Configuration(new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build()).WriteTo.Console()
+            .CreateLogger();
 
-        builder.Services.AddControllers().AddJsonOptions(options =>
+        try
         {
-            options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
-        });
-        builder.Services.AddEndpointsApiExplorer();
-        builder.Services.AddDatabase(configuration);
-        builder.Services.AddSwaggerGen();
-        builder.Services.AddApplication();
-        
-        var app = builder.Build();
+            var builder = WebApplication.CreateBuilder(args);
+            var configuration = builder.Configuration;
 
-        if (app.Environment.IsDevelopment())
-        {
-            app.UseSwagger();
-            app.UseSwaggerUI();
+            builder.Host.UseSerilog();
+            builder.Services.AddControllers().AddJsonOptions(options =>
+            {
+                options.JsonSerializerOptions.Converters.Add(new JsonStringEnumConverter());
+            });
+            builder.Services.AddProblemDetails();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddDatabase(configuration);
+            builder.Services.AddApplication();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.MapControllers();
+            // app.UseExceptionHandler();
+
+            Log.Information("Application started successfully");
+            app.Run();
         }
-
-        app.UseHttpsRedirection();
-        app.MapControllers();
-        // app.UseExceptionHandler();
-        
-        app.Run();
+        catch (Exception ex)
+        {
+            Log.Fatal(ex, "Application terminated unexpectedly");
+        }
+        finally
+        {
+            Log.CloseAndFlush();
+        }
     }
 }
