a8aebc565c81164f7fb29d8a46f810a699306637
olegg <s.gelo2004@gmail.com>
2025-07-11 21:18:18 +0300

    chore: remove redundant annotation

--- src/BiogenomTest.Domain/Entities/IntakeProjection.cs
+++ src/BiogenomTest.Domain/Entities/IntakeProjection.cs
@@ -1,15 +1,13 @@
-﻿using System.ComponentModel.DataAnnotations.Schema;
-
-namespace BiogenomTest.Domain.Entities;
+﻿namespace BiogenomTest.Domain.Entities;
 
 //нов. потреб. с уч. набора
 public class IntakeProjection
 {
     public int Id { get; set; }
-
-    [ForeignKey("DailyIntake")]
+    
     public int DailyIntakeId { get; set; }
     public DailyIntake DailyIntake { get; set; } = null!;
+    
     
     public double FromSet { get; set; }
     public double FromFood { get; set; }
